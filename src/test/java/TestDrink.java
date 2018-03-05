658db11d0d31524276bcfaa075b5601b2a84c55b
John McCollum <johnvmccollum@gmail.com>
2018-03-05 11:54:28 +0000

    Test Drink can get name

--- src/test/java/TestDrink.java
+++ src/test/java/TestDrink.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestDrink {
     private Drink drink;
@@ -6,5 +9,10 @@ public class TestDrink {
     @Before
     public void setUp() throws Exception {
         drink = new Drink("Pepsi", 330);
+    }
+
+    @Test
+    public void testCanGetName() {
+        assertEquals("Pepsi", drink.getName());
     }
 }
