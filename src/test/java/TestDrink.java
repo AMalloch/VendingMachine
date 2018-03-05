d0064a2479d30812e5e4869c61090277baf58c7e
John McCollum <johnvmccollum@gmail.com>
2018-03-05 11:53:41 +0000

    Adds setup in Drink test

--- src/test/java/TestDrink.java
+++ src/test/java/TestDrink.java
@@ -1,2 +1,10 @@
+import org.junit.Before;
+
 public class TestDrink {
+    private Drink drink;
+
+    @Before
+    public void setUp() throws Exception {
+        drink = new Drink("Pepsi", 330);
+    }
 }
